8ecef2ad2b4dc8baa7736b0dc125dfa5f665248c
Pedro <pedrohenriiquez.2008@gmail.com>
2025-11-26 15:49:39 -0300

    26/11 pistola funcional

---
+++ Assets/pistolaProyectiles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pistolaProyectiles : MonoBehaviour
+{
+    public Transform firePoint;        //punto desde donde sale el proyectil (la punta de la pistola)
+    public GameObject proyectilPrefab;    //prefab de la esfera
+
+    [SerializeField] float fuerzaProyectil;   //fuerza con la que sale el proyectil
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Disparar();
+        }
+    }
+
+    void Disparar()
+    {
+        GameObject proyectil = Instantiate(proyectilPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = proyectil.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.forward * fuerzaProyectil);
+    }
+}
